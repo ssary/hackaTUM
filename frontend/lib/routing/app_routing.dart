c564a96395fb3b5bfd4f95a16608883a446b3102
Henri <henri.breuer@outlook.de>
2024-11-23 11:48:58 +0100

    add basic pages and navigation

--- frontend/lib/routing/app_routing.dart
+++ frontend/lib/routing/app_routing.dart
@@ -1,3 +1,5 @@
+import 'package:frontend/screens/activity_details/activity_details_screen.dart';
+import 'package:frontend/screens/activity_selection/activity_selection_screen.dart';
 import 'package:frontend/screens/create_activity/create_activity_screen.dart';
 import 'package:frontend/screens/home_screen/home_screen.dart';
 import 'package:go_router/go_router.dart';
@@ -6,6 +8,9 @@ class AppRouting {
   // static names
   static const home = '/';
   static const createActivity = '/create-activity';
+  static const selectActivity = '/select-activity';
+  static const activityDetails = '/activity-details';
+  static const error404 = '/error-404';
   // static const variable = "/location"
 
   static final router = GoRouter(
@@ -24,7 +29,17 @@ class AppRouting {
             GoRoute(
                 name: createActivity,
                 path: createActivity,
-                builder: (context, state) => const CreateActivityScreen())
+                builder: (context, state) => const CreateActivityScreen()),
+            GoRoute(
+              name: selectActivity,
+              path: selectActivity,
+              builder: (context, state) => const ActivitySelectionScreen(),
+            ),
+            GoRoute(
+              name: activityDetails,
+              path: activityDetails,
+              builder: (context, state) => const ActivityDetailsScreen(),
+            )
           ])
     ],
   );
---
+++ frontend/lib/screens/activity_details/activity_details_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/common_widgets/solid_button.dart';
+import 'package:frontend/constants/app_spacing.dart';
+import 'package:frontend/routing/app_routing.dart';
+import 'package:frontend/screens/activity_selection/activity_selection_screen.dart';
+import 'package:frontend/theme/colors.dart';
+import 'package:go_router/go_router.dart';
+
+class ActivityDetailsScreen extends StatefulWidget {
+  const ActivityDetailsScreen({super.key});
+
+  @override
+  State<ActivityDetailsScreen> createState() => _ActivityDetailsScreenState();
+}
+
+class _ActivityDetailsScreenState extends State<ActivityDetailsScreen> {
+  //TODO
+  List peopleJoined = ["Person 1", "Person 2", "Person 3"];
+
+  late double screenHeight;
+  late double screenWidth;
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 400,
+            width: screenWidth,
+            color: Colors.lightBlue,
+            child: Text("Your activity"),
+          ),
+          gapH32,
+          Text("Waiting Room"),
+          ListView.builder(
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("Person ${peopleJoined[index]}"),
+                );
+              },
+              itemCount: 3),
+          gapH24,
+          SolidButton(
+              text: "Open Chat",
+              backgroundColor: Colors.blue,
+              onPressed: () {}),
+          gapH24,
+          SolidButton(
+              text: "Cancel Activity",
+              backgroundColor: AppColors.errorRed,
+              onPressed: () {
+                //TODO:
+                context.goNamed(AppRouting.home);
+              })
+        ],
+      ),
+    );
+  }
+}
