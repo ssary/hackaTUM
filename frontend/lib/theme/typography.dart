c94c452775d30307132cdc81a320155841e5ad7f
Henri <henri.breuer@outlook.de>
2024-11-23 11:59:20 +0100

    add font code

--- frontend/lib/theme/typography.dart
+++ frontend/lib/theme/typography.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTypography {
-  static TextTheme textTheme = GoogleFonts.poppinsTextTheme();
+  static TextTheme textTheme = GoogleFonts.soraTextTheme();
 }
