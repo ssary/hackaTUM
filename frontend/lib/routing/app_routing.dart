98ae2861b1ae7e9d71e415bf378bfdfc98d47091
Henri <henri.breuer@outlook.de>
2024-11-23 10:57:51 +0100

    add home page layout

--- frontend/lib/routing/app_routing.dart
+++ frontend/lib/routing/app_routing.dart
@@ -1,9 +1,11 @@
+import 'package:frontend/screens/create_activity/create_activity_screen.dart';
 import 'package:frontend/screens/home_screen/home_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouting {
   // static names
   static const home = '/';
+  static const createActivity = '/create-activity';
   // static const variable = "/location"
 
   static final router = GoRouter(
@@ -13,11 +15,16 @@ class AppRouting {
           name: home,
           path: home,
           builder: (context, state) => const HomeScreen(),
-          routes: const [
+          routes: [
             /*GoRoute(
               name: variable,
               path: variable
               builder: (context, state) => VariableScreen(state.params['location']))*/
+
+            GoRoute(
+                name: createActivity,
+                path: createActivity,
+                builder: (context, state) => const CreateActivityScreen())
           ])
     ],
   );
--- frontend/lib/screens/home_screen/home_screen.dart
+++ frontend/lib/screens/home_screen/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/common_widgets/solid_button.dart';
+import 'package:frontend/constants/app_spacing.dart';
+import 'package:frontend/routing/app_routing.dart';
+import 'package:frontend/theme/colors.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,18 +16,43 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Home',
-            ),
-          ],
-        ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(Spacing.p8),
+                    child: Icon(
+                      Icons.person,
+                      size: 48,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(Spacing.p8),
+                    child: Icon(
+                      Icons.list,
+                      size: 48,
+                    ),
+                  ),
+                ],
+              ),
+              gapH104,
+              IconButton(
+                  onPressed: () {
+                    context.goNamed(AppRouting.createActivity);
+                  },
+                  icon: Icon(Icons.circle_rounded, size: 300)),
+              gapH16,
+              SolidButton(
+                  text: "Choose from past activities",
+                  backgroundColor: AppColors.primaryColor,
+                  onPressed: () {})
+            ],
+          ),
+        ],
       ),
     );
   }
